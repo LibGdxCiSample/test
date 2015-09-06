630356a9a1f53961e12e17508fe1392b878d5105
nicofr69 <nicofr69.github@gmail.com>
2015-09-06 15:55:11 +0200

    initial upload
    libgdx default program (badlogic logo)

---
+++ desktop/src/com/sample/libgdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sample.libgdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sample.libgdx.game.MySampleLibgdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MySampleLibgdxGame(), config);
+	}
+}
---
+++ html/src/com/sample/libgdx/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.sample.libgdx.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.sample.libgdx.game.MySampleLibgdxGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new MySampleLibgdxGame();
+        }
+}
